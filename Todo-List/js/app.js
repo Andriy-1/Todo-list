910ceef6e64dc2ec0ecc29b7c36eecc509a93a87
Andrii <andriy.hava2@gmail.com>
2021-07-27 23:26:29 +0300

    Remove excess code

--- Todo-List/js/app.js
+++ Todo-List/js/app.js
@@ -1,3 +1,27 @@
+function body_lock_add(delay) {
+	let body = document.querySelector("body");
+	
+}
+
+let unlock = true;
+
+//=================
+//ActionsOnHash
+if (location.hash) {
+	const hsh = location.hash.replace('#', '');
+	if (document.querySelector('.popup_' + hsh)) {
+		popup_open(hsh);
+	} else if (document.querySelector('div.' + hsh)) {
+		_goto(document.querySelector('.' + hsh), 500, '');
+	}
+}
+//=================
+
+
+//BodyLock
+
+
+//=================
 
 //=================
 //Popups
